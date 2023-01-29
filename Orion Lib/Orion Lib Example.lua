e236d2d2d1d6eb75b5c755f6a43a025cf3c07cd4
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-01-29 15:34:05 +0300

    Update Orion Lib Example.lua

--- Orion Lib/Orion Lib Example.lua
+++ Orion Lib/Orion Lib Example.lua
@@ -1,14 +1,14 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
 
 OrionLib:MakeNotification({
-	Name = "Krypted Hub",
-	Content = "Welcome To Krypted Hub! Have Fun!",
+	Name = "Wreston Hub", 
+	Content = "Welcome To Wreston Hub! Have Fun!",
 	Image = "rbxassetid://4483345998",
 	Time = 5
 })
 
 
-local Window = OrionLib:MakeWindow({Name = "Krypted Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "KryptedSaves"})
+local Window = OrionLib:MakeWindow({Name = "Wreston Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "KryptedSaves"})
 
 --Player Tab--
 
@@ -51,7 +51,7 @@ local SettingsSection = SettingsTab:AddSection({
 })
 
 SettingsSection:AddButton({
-	Name = "Destroy UI",
+	Name = "Close UI",
 	Callback = function()
         OrionLib:Destroy()
   	end    
