2ff4a84add608b8ad3338d0654978f6640f27163
xtended <83477843+drillygzzly@users.noreply.github.com>
2023-03-13 01:32:57 -0400

    Add files via upload

---
+++ Tokyo Lib/Tokyo Lib Example.lua
@@ -0,0 +1,176 @@
+local Decimals = 4
+local Clock = os.clock()
+local ValueText = "Value Is Now :"
+
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/dohmai/Tokyo/main/Libraries/UI"))({
+    cheatname = "Title Here", -- watermark text
+    gamename = "gzz#2600", -- watermark text
+})
+
+library:init()
+
+local Window1  = library.NewWindow({
+    title = "Title Here | Title Here", -- Mainwindow Text
+    size = UDim2.new(0, 510, 0.6, 6
+)})
+
+local Tab1 = Window1:AddTab("  Tab1  ")
+local SettingsTab = library:CreateSettingsTab(Window1)
+
+--Tab1:SetText("Text")
+
+local Section1 = Tab1:AddSection("Section 1", 1)
+--Section1:SetText("Text")
+
+Section1:AddToggle({
+    text = "Toggle1",
+    state = false,
+    risky = true,
+    tooltip = "tooltip1",
+    flag = "Toggle_1",
+    risky = false,
+    callback = function(v)
+        print(ValueText, v)
+    end
+}):AddBind({
+    enabled = true,
+    text = "Keybind1",
+    tooltip = "tooltip1",
+    mode = "toggle",
+    bind = "None",
+    flag = "ToggleKey_1",
+    state = false,
+    nomouse = false,
+    risky = false,
+    noindicator = false,
+    callback = function(v)
+        print(ValueText, v)
+    end,
+    keycallback = function(v)
+        print(ValueText, v)
+    end
+})
+
+Section1:AddBox({
+    enabled = true,
+    name = "TextBox1",
+    flag = "TextBox_1",
+    input = "PlaceHolder1",
+    focused = false,
+    risky = false,
+    callback = function(v)
+        print(ValueText, v)
+    end
+})
+
+--[[Section1:AddToggle({
+    text = "Toggle1",
+    flag = "Toggle_1",
+    callback = function(v)
+        print(ValueText, v)
+    end
+}):AddColor({
+    text = "Color1",
+    color = Color3.fromRGB(255, 255, 255),
+    flag = "Color_1",
+    callback = function(v)
+        print(ValueText, v)
+    end
+})--]]
+
+--[[Section1:AddBind({
+    enabled = true,
+    text = "Keybind1",
+    tooltip = "tooltip1",
+    mode = "toggle",
+    bind = "None",
+    flag = "ToggleKey_1",
+    state = false,
+    nomouse = false,
+    risky = false,
+    noindicator = false,
+    callback = function(v)
+        print(ValueText, v)
+    end,
+    keycallback = function(v)
+        print(ValueText, v)
+    end
+})--]]
+
+Section1:AddSeparator({
+    enabled = true,
+    text = "Separator1"
+})
+
+Section1:AddButton({
+    enabled = true,
+    text = "Button1",
+    tooltip = "tooltip1",
+    confirm = true,
+    risky = false,
+    callback = function()
+        print("Pressed!")
+    end
+})
+
+-- Button1:SetText("Text")
+
+Section1:AddSlider({
+    enabled = true,
+    text = "Slider1",
+    tooltip = "tooltip1",
+    flag = "Slider_1",
+    suffix = "",
+    dragging = true,
+    focused = false,
+    min = 0,
+    max = 100,
+    increment = 0.1,
+    risky = false,
+    callback = function(v)
+        print(ValueText, v)
+    end
+})
+
+--Slider_1:SetValue(100)
+
+Section1:AddList({
+    enabled = true,
+    text = "Selection", 
+    tooltip = "tooltip1",
+    selected = "",
+    multi = false,
+    open = false,
+    max = 4,
+    values = {"1", "2", "3"},
+    risky = false,
+    callback = function(v)
+        print(ValueText, v)
+    end
+})
+
+Section1:AddColor({
+    enabled = true,
+    text = "Color1",
+    tooltip = "tooltip1",
+    color = Color3.fromRGB(255, 255, 255),
+    flag = "Color_1",
+    trans = 0,
+    open = false,
+    risky = false,
+    callback = function(v)
+        print(ValueText, v)
+    end
+})
+
+local Time = (string.format("%."..tostring(Decimals).."f", os.clock() - Clock))
+library:SendNotification(("Loaded In "..tostring(Time)), 6)
+
+--[[
+    --Window1:SetOpen(false)
+    makefolder("Title Here")
+    library:SetTheme(Default)
+    library:GetConfig(Default)
+    library:LoadConfig(Default)
+    library:SaveConfig(Default)
+]]
